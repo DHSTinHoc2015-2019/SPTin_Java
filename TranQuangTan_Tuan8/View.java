98b6a74e8014235b9b976146b5f75c9a7c291759
tranquangtanqt <tranquangtanqt1990@gmail.com>
2018-05-28 00:03:21 +0700

    Create View.java

---
+++ TranQuangTan_Tuan8/View.java
@@ -0,0 +1,13 @@
+package javaTuan8;
+
+import javax.swing.JFrame;
+
+public class View {
+
+	public static void main(String[] args) {
+		MyFrame frame = new MyFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+	}
+}
